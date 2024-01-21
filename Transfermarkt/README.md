29ac5b91d848f29045dff9b127d7aed36417a744
Aleksei Zhukov <59407868+az38@users.noreply.github.com>
2024-01-21 09:54:12 +0100

    Initial commit

---
+++ Transfermarkt/README.md
@@ -0,0 +1,11 @@
+# End-To-End Azure Graph SQL analytics
+
+Transfermarkt example
+
+For the project the data and technology from:
+https://rapidapi.com/ (https://rapidapi.com/apidojo/api/transfermarket)
+https://www.transfermarkt.com/
+
+were used.
+If any license restriction or mentioning should be used - please contact the author.
+Project has clear non-commercial purposes
---
+++ Transfermarkt/src/03_Query.sql
@@ -0,0 +1,16 @@
+select 
+distinct
+    -- need to exlucde ooposite ways
+    case when Clubs1.Name < Clubs2.Name then Clubs1.Name else Clubs2.Name end as club1
+    , case when Clubs1.Name < Clubs2.Name then Clubs2.Name else Clubs1.Name end as club2
+    , Players.Name as Player
+from 
+    Players
+    , Squad as Squad1
+    , Squad as Squad2 
+    , Clubs as Clubs1
+    , Clubs as Clubs2
+where 
+    MATCH (Clubs1-(Squad1)->Players<-(Squad2)-Clubs2) 
+    and Clubs2.Name != Clubs1.Name
+
